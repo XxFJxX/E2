21501bd1c217ac2913191f140bd99c3892bf6543
XxFJxX <ferinocente357@gmail.com>
2023-06-30 09:42:06 -0600

    Agregar archivos de proyecto.

---
+++ E2/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace E2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ E2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace E2;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        int A1 = Convert.ToInt32(A.Text);
+        int B1 = Convert.ToInt32(B.Text);
+        int r = A1 + B1;
+        int y = (r * r) / 2;
+
+        la.Text = "(" + A1 + "+" + B1 + ")" + "^2/2" + " = " + y;
+    }
+}
+
